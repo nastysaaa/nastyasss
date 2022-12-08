0f6dd8b3d07fb5b00da0bf1c150532f93df00507
nastysaaa <115701813+nastysaaa@users.noreply.github.com>
2022-12-08 12:18:58 +0200

    1111

---
+++ pudds.py
@@ -0,0 +1,6 @@
+summa = 0
+skaitlis = int(input("Ievadi skaitli="))
+while skaitlis != 0:
+    summa += skaitlis 
+    skaitlis = int(input("Ievadi skaitli="))
+print("Ievadīto skaitļu summa ir", summa)
