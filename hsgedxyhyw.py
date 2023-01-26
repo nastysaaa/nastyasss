a5e40d78b0dd9cbe603ae147c65d92a5c0d6620c
nastysaaa <115701813+nastysaaa@users.noreply.github.com>
2023-01-26 12:43:21 +0200

    9666

---
+++ hsgedxyhyw.py
@@ -0,0 +1,12 @@
+import random
+#random.seed(124)
+sk1 = random.random()
+sk2 = random.uniform (5.0, 10.0)
+sk3 = random.randint(1, 5)
+sk4 = random.randrange(5.0, 10.0)
+izvele = random.choice('ABCDEF')
+print("Gadijuma skaitlis=", sk1)
+print("Gadijuma skaitlis=", sk2)
+print("Gadijuma skaitlis=", sk3)
+print("Gadijuma skaitlis=", sk4)
+print("Gadijuma skaitlis=", izvele)
